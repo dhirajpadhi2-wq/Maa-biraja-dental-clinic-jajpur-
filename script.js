0a32d8170ae1d3ef866317c2afc693830266e41b
dhirajpadhi2-wq <dhirajpadhi2@gmail.com>
2026-03-01 20:18:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-AOS.init();
 
-function openForm(){
-document.getElementById("appointmentForm").style.display="flex";
-}
-
-function closeForm(){
-document.getElementById("appointmentForm").style.display="none";
-}
